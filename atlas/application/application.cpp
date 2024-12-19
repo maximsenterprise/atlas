faecc553fce062d54bb90a0cfceb7e6032c89317
Max Van den Eynde <maxvdec@gmail.com>
2024-12-19 07:14:46 +0100

    Scaffolded project --------------------------------------- Scaffolded main project and created configuration files for building the project.
    Signed-off-by: Max Van den Eynde <maxvdec@gmail.com>

---
+++ atlas/application/application.cpp
@@ -0,0 +1,8 @@
+/*
+* application.cpp
+* As part of the Atlas project
+* Created by Maxims Enterprise in 2024
+* --------------------------------------
+* Description: Main application class for atlas
+* Copyright (c) 2024 Maxims Enterprise
+*/
---
+++ atlas_test/main.cpp
@@ -0,0 +1,8 @@
+/*
+* main.cpp
+* As part of the Atlas project
+* Created by Maxims Enterprise in 2024
+* --------------------------------------
+* Description: Main entry point for atlas testing
+* Copyright (c) 2024 Maxims Enterprise
+*/
